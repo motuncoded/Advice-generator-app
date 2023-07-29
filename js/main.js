383fd39c0ba41518e747003563c0f532bd1e61fb
motuncoded <motunrayoadeneye@example.com>
2023-07-29 21:48:48 +0100

    added functionality

--- js/main.js
+++ js/main.js
@@ -2,17 +2,29 @@ const btn = document.querySelector("button");
 const quote = document.getElementById ("quote");
 const adviceId = document.getElementById("adviceId");
 const api = "https://api.adviceslip.com/advice";
+const dice = document.querySelector("#dice")
 
+const displayRotation = () =>{
+    dice.classList.add("rotate");
+    setTimeout(()=>{
+        dice.classList.remove("rotate")
+    }, 1000)
 
+}
+const hideRotation= ()=>{
+    dice.classList.remove("rotate")
+
+}
 
 
 
 function fetchApi(api){
-
-    fetch("https://api.adviceslip.com/advice", { cache: "no-cache" }).then((response) =>{
+displayRotation()
+    fetch("https://api.adviceslip.com/advice").then((response) =>{
     return response.json();
 })
 .then((data)=>{
+    hideRotation()
     const adviceMessage = data["slip"];
     adviceId.innerHTML = adviceMessage["id"];
     quote.innerHTML = adviceMessage["advice"];
@@ -22,7 +34,9 @@ function fetchApi(api){
 
 btn.addEventListener("click", ()=>{
     fetchApi()
+    
 })
+
 
 
 
